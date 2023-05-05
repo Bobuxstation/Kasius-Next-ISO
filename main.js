2fb3f027e0101dbbb9b27a5badda8dda5c437ccc
BuildTools <unconfigured@null.spigotmc.org>
2023-05-05 21:16:05 +0700

    resolution fixes

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
 const electron = require('electron')
-const {app, BrowserWindow, ipcMain, ipcRenderer} = require("electron");
+const { app, BrowserWindow, ipcMain, ipcRenderer, screen } = require("electron");
 const fs = require('fs');
 const path = require('path');
 
-function createWindow () {
+function createWindow() {
 
-    const win = new BrowserWindow({
-      icon: './costume2.png',
-      autoHideMenuBar: true,
-      fullscreen: true,
-      webPreferences: {
-        enableRemoteModule: true,
-        nodeIntegration: true,
-        devTools: true,
-        contextIsolation: false,
-        nodeIntegrationInSubFrames:true,
-        nodeIntegrationInWorker: true,
-        webviewTag:true
-      }
-    })
-    require('@electron/remote/main').initialize()
-    require('@electron/remote/main').enable(win.webContents)
-  
-    win.loadFile('index.html')
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const win = new BrowserWindow({
+    icon: './costume2.png',
+    autoHideMenuBar: true,
+    width: width,
+    height: height,
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      devTools: true,
+      contextIsolation: false,
+      nodeIntegrationInSubFrames: true,
+      nodeIntegrationInWorker: true,
+      webviewTag: true
+    }
+  })
+  require('@electron/remote/main').initialize()
+  require('@electron/remote/main').enable(win.webContents)
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
+})
 
-  app.whenReady().then(createWindow)
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-  
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
